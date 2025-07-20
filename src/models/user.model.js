46b9fbc75e5c35fc2cd45568dea56efb4b3c74ec
ArpitChhabra23306 <arpitchhabra9692@gmail.com>
2025-07-20 16:30:29 +0530

    models done of user and video

---
+++ src/models/user.model.js
@@ -0,0 +1,101 @@
+import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true //perfomance decrease but helps in searching
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, //claudnary url
+            required: true,
+        },
+        coverImage: {
+            type: String, //claudnary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+userSchema.pre("save", async function(next) {
+    if (this.isModified("password")){       // If password is not modified, skip hashing
+        this.password = await bcrypt.hash(this.password, 10);       // Hash the password before saving the user
+        next()
+    } else {
+        next()       // If password is not modified, just proceed to the next middleware      
+    }    
+})        // Hash password before saving user, pre does your work/func just before doing the task(here data saving)
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password); // Compare the provided password with the hashed password
+}    
+
+
+userSchema.methods.generateAccessToken = function() {
+    return jwt.sign(
+        { //sign method is used to generate token
+            _id: this._id,
+            username: this.username,
+            email: this.email,
+            fullName: this.fullName,
+        },
+        process.env.ACCESS_TOKEN_SECRET, //secret key
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        } 
+
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        { //sign method is used to generate token
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET, //secret key
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        } 
+
+    )
+}
+
+
+
+export const User = mongoose.nodel("User",userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+import mongoose,{Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-paginate-v2"
+
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //claudnary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, //claudnary url
+            required: true,
+        },
+        title: {
+            type: String, 
+            required: true,
+        },
+        description: {
+            type: String, 
+            required: true,
+        },
+        duration: {
+            type: Number, //claudnary url
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate); // Add pagination plugin to the schema, this allows us to use aggregate with pagination
+
+
+
+export const Video = mongoose.model("Video",videoSchema)
