9bbb56a02bb4d53ca70b2026a005d97823e32613
ArpitChhabra23306 <arpitchhabra9692@gmail.com>
2025-08-26 16:26:02 +0530

    feat: add user watch history retrieval and update account routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 
 app.use(cors({
@@ -14,7 +13,6 @@ app.use(express.json({limit: "1mb"})); //to parse json data from request
 app.use(express.urlencoded({extended: true, limit: "1mb"})); //url encoder, taking data from url
 app.use(express.static("public")); //to serve static files
 app.use(cookieParser());  //to parse cookies from request
-
 
 // Importing routes
 import userRouter from "./routes/user.router.js";
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurreentPassword } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurreentPassword, getCurrrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"; // Assuming multer is used for file uploads
 import { verifyJWt } from "../middlewares/auth.middleware.js";
 
@@ -30,7 +30,12 @@ router.route("/logout").post(verifyJWt, logoutUser); //thats why next was used i
 router.route("/refresh-token").post(refreshAccessToken);
 
 router.post("/change-password", verifyJWt, changeCurreentPassword);
-
+router.post("/current-user",verifyJWt, getCurrrentUser);
+router.route("/update-account").patch(verifyJWt,updateAccountDetails);
+router.route("/avatar").patch(verifyJWt, upload.single("avatar"), updateAvatar);
+router.route("/cover-image").patch(verifyJWt, upload.single("coverImage"), updateCoverImage);
+router.route("/c/:username").get(verifyJWt,getUserChannelProfile); //c for channel, passing username as param
+router.route("/history").get(verifyJWt, getWatchHistory); //to get user watch history
 
 console.log("User router executing");
 
