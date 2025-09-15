d340df69c40f1512e31af2d78396214edaf61821
ArpitChhabra23306 <arpitchhabra9692@gmail.com>
2025-09-15 18:41:10 +0530

    video controller pubishAVideo working

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,7 +1,7 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Video} from "../models/video.model.js"
 import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
+import ApiErrors from "../utils/ApiErrors.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
@@ -13,44 +13,39 @@ const getAllVideos = asyncHandler(async (req, res) => {
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
-    
-    // TODO: get video, upload to cloudinary, create video
-    const { title, description} = req.body;
-    if(title, description){
+    const { title, description } = req.body;
+    if (!title || !description) {
         throw new ApiError(400, "Title and description are required");
     }
 
-    //taking files from multer from route callback
     const videoFileLocalPath = req.files?.videoFile?.[0]?.path;
     const thumbnailLocalPath = req.files?.thumbnail?.[0]?.path;
-    //checking if file is present or not
-    if(!videoFileLocalPath || !thumbnailLocalPath){
+
+    if (!videoFileLocalPath || !thumbnailLocalPath) {
         throw new ApiError(400, "Video file and thumbnail are required");
     }
 
-    //uploading on cloudinary
     const videoFile = await uploadOnCloudinary(videoFileLocalPath);
     const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
-    //checking upload success
-    if(!videoFile?.url || !thumbnail?.url){
+
+    if (!videoFile?.url || !thumbnail?.url) {
         throw new ApiError(500, "Error in uploading files, please try again");
     }
 
-    //creating video document in database
     const video = await Video.create({
         title,
         description,
         videoFile: videoFile.url,
         thumbnail: thumbnail.url,
-        duration: videoFile.duration,
-        owner: req.user._id //from JWT
+        duration: videoFile.duration || 0, // fallback if duration not returned
+        owner: req.user._id
     });
 
-    // sending response
     return res
         .status(201)
         .json(new ApiResponse(201, video, "Video published successfully"));
-})
+});
+
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import mongoosePaginate from "mongoose-paginate-v2";
+
 
 const commentSchema = new Schema(
     {
@@ -22,6 +23,6 @@ const commentSchema = new Schema(
 )
 
 
-commentSchema.plugin(mongooseAggregatePaginate)
+commentSchema.plugin(mongoosePaginate)
 
 export const Comment = mongoose.model("Comment", commentSchema)
